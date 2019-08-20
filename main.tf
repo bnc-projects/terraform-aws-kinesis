9683e98dd197c41327b636bb4d49ed255631b308
Vincent Wen <vincent@bravenewcoin.com>
2019-08-21 11:36:36 +1200

    Add Terraform code to implement CloudWatch Alarm

--- main.tf
+++ main.tf
@@ -1,9 +1,39 @@
 resource "aws_kinesis_stream" "default" {
-  name             = var.kinesis_stream_name
-  shard_count      = var.shard_count
-  retention_period = var.retention_period
+  name                = var.kinesis_stream_name
+  shard_count         = var.shard_count
+  retention_period    = var.retention_period
   shard_level_metrics = var.shard_level_metrics
-  encryption_type = var.encryption_type
-  kms_key_id = var.kms_key_id
-  tags = var.tags
+  encryption_type     = var.encryption_type
+  kms_key_id          = var.kms_key_id
+  tags                = var.tags
+}
+
+resource "aws_cloudwatch_metric_alarm" "read_throughput_exceeded" {
+  alarm_name          = var.read_throughput_exceeded_name
+  comparison_operator = var.read_throughput_comparison_operator
+  evaluation_periods  = var.read_throughput_evaluation_periods
+  metric_name         = var.read_throughput_metric_name
+  namespace           = var.read_throughput_namespace
+  period              = var.read_throughput_period
+  statistic           = var.read_throughput_statistic
+  threshold           = var.read_throughput_threshold
+  alarm_description   = var.read_throughput_alarm_description
+  treat_missing_data  = var.read_throughput_treat_missing_data
+  dimensions          = var.read_throughput_dimensions
+  alarm_actions       = var.read_throughput_alarm_actions
+}
+
+resource "aws_cloudwatch_metric_alarm" "write_throughput_exceeded" {
+  alarm_name          = var.write_throughput_exceeded_name
+  comparison_operator = var.write_throughput_comparison_operator
+  evaluation_periods  = var.write_throughput_evaluation_periods
+  metric_name         = var.write_throughput_metric_name
+  namespace           = var.write_throughput_namespace
+  period              = var.write_throughput_period
+  statistic           = var.write_throughput_statistic
+  threshold           = var.write_throughput_threshold
+  alarm_description   = var.write_throughput_alarm_description
+  treat_missing_data  = var.write_throughput_treat_missing_data
+  dimensions          = var.write_throughput_dimensions
+  alarm_actions       = var.write_throughput_alarm_actions
 }
--- variables.tf
+++ variables.tf
@@ -38,3 +38,150 @@ variable "tags" {
   description = "A map of tags to add to all resources"
   default     = {}
 }
+
+variable "read_throughput_exceeded_name" {
+  type        = string
+  description = "The descriptive name for the alarm."
+}
+
+variable "write_throughput_exceeded_name" {
+  type        = string
+  description = "The descriptive name for the alarm."
+}
+
+variable "read_throughput_comparison_operator" {
+  type        = string
+  description = "The arithmetic operation to use when comparing the specified Statistic and Threshold."
+  default     = "GreaterThanOrEqualToThreshold"
+}
+
+variable "write_throughput_comparison_operator" {
+  type        = string
+  description = "The arithmetic operation to use when comparing the specified Statistic and Threshold."
+  default     = "GreaterThanOrEqualToThreshold"
+}
+
+variable "read_throughput_evaluation_periods" {
+  type        = number
+  description = "The number of periods over which data is compared to the specified threshold."
+  default     = 1
+}
+
+variable "write_throughput_evaluation_periods" {
+  type        = number
+  description = "The number of periods over which data is compared to the specified threshold."
+  default     = 1
+}
+
+variable "read_throughput_metric_name" {
+  type        = string
+  description = "The name for the alarm's associated metric. See docs for supported metrics."
+  default     = "ReadProvisionedThroughputExceeded"
+}
+
+variable "write_throughput_metric_name" {
+  type        = string
+  description = "The name for the alarm's associated metric. See docs for supported metrics."
+  default     = "WriteProvisionedThroughputExceeded"
+}
+
+variable "read_throughput_namespace" {
+  type        = string
+  description = "The namespace for the alarm's associated metric. See docs for the list of namespaces."
+  default     = "AWS/Kinesis"
+}
+
+variable "write_throughput_namespace" {
+  type        = string
+  description = "The namespace for the alarm's associated metric. See docs for the list of namespaces."
+  default     = "AWS/Kinesis"
+}
+
+variable "read_throughput_period" {
+  type        = number
+  description = "The period in seconds over which the specified statistic is applied."
+  default     = 60
+}
+
+variable "write_throughput_period" {
+  type        = number
+  description = "The period in seconds over which the specified statistic is applied."
+  default     = 60
+}
+
+variable "read_throughput_statistic" {
+  type        = string
+  description = "The statistic to apply to the alarm's associated metric. Either of the following is supported: SampleCount, Average, Sum, Minimum, Maximum"
+  default     = "Average"
+}
+
+variable "write_throughput_statistic" {
+  type        = string
+  description = "The statistic to apply to the alarm's associated metric. Either of the following is supported: SampleCount, Average, Sum, Minimum, Maximum"
+  default     = "Average"
+}
+
+variable "read_throughput_threshold" {
+  type        = number
+  description = "The value against which the specified statistic is compared."
+  default     = 0
+}
+
+variable "write_throughput_threshold" {
+  type        = number
+  description = "The value against which the specified statistic is compared."
+  default     = 0
+}
+
+variable "read_throughput_alarm_description" {
+  type        = string
+  description = "alarm description"
+  default     = "Alarm when read throughput is exceeded"
+}
+
+variable "write_throughput_alarm_description" {
+  type        = string
+  description = "alarm description"
+  default     = "Alarm when write throughput is exceeded"
+}
+
+variable "read_throughput_alarm_actions" {
+  type        = list
+  description = "The list of actions to execute when this alarm transitions into an ALARM state from any other state. Each action is specified as an Amazon Resource Name (ARN)."
+}
+
+variable "write_throughput_alarm_actions" {
+  type        = list
+  description = "The list of actions to execute when this alarm transitions into an ALARM state from any other state. Each action is specified as an Amazon Resource Name (ARN)."
+}
+
+variable "read_throughput_dimensions" {
+  type        = map
+  description = "The dimensions for the alarm's associated metric."
+  default     = {}
+}
+
+variable "write_throughput_dimensions" {
+  type        = map
+  description = "The dimensions for the alarm's associated metric."
+  default     = {}
+}
+
+variable "read_throughput_treat_missing_data" {
+  type        = string
+  description = "Sets how this alarm is to handle missing data points. The following values are supported: missing, ignore, breaching and notBreaching. Defaults to missing"
+  default     = "missing"
+}
+
+variable "write_throughput_treat_missing_data" {
+  type        = string
+  description = "Sets how this alarm is to handle missing data points. The following values are supported: missing, ignore, breaching and notBreaching. Defaults to missing"
+  default     = "missing"
+}
+
+
+
+
+
+
+
