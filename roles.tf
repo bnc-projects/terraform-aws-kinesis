1cddbc62319cd14564207bfd2fa5b076385f48bc
Yi Zhang <yi@bravenewcoin.com>
2019-08-27 14:15:39 +1200

    Attach policy to role (#6)
    * Attach policy to role

--- roles.tf
+++ roles.tf
@@ -1,9 +1,16 @@
 resource "aws_iam_role" "firehose_access_role" {
   count              = local.count
   name               = format("%s-kinesis-stream-role", var.kinesis-firehose_name)
-  assume_role_policy = data.aws_iam_policy_document.read_from_kinesis_stream[0].json
+  assume_role_policy = data.aws_iam_policy_document.firehose_delivery_assume_policy[0].json
   tags               = var.tags
+}
 
+resource "aws_iam_role_policy" "firehose_access_policy" {
+  count  = local.count
+  name   = "allow_firehose_read_from_kinesis_stream"
+  role   = aws_iam_role.firehose_access_role[0].id
+  policy = data.aws_iam_policy_document.read_from_kinesis_stream[0].json
+  tags   = var.tags
 }
 
 data "aws_iam_policy_document" "read_from_kinesis_stream" {
@@ -24,14 +31,41 @@ data "aws_iam_policy_document" "read_from_kinesis_stream" {
   }
 }
 
+data "aws_iam_policy_document" "firehose_delivery_assume_policy" {
+  count = local.count
+  statement {
+    sid    = "AllowFirehoseToAssumeRole"
+    effect = "Allow"
+
+    actions = [
+      "sts:AssumeRole"
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = [
+        "firehose.amazonaws.com"
+      ]
+    }
+  }
+}
+
 resource "aws_iam_role" "firehose_delivery_role" {
   count              = local.count
   name               = format("%s-s3-role", var.kinesis-firehose_name)
-  assume_role_policy = data.aws_iam_policy_document.firehose_delivery_policy[0].json
+  assume_role_policy = data.aws_iam_policy_document.firehose_delivery_assume_policy[0].json
   tags               = var.tags
 }
 
-data "aws_iam_policy_document" "firehose_delivery_policy" {
+resource "aws_iam_role_policy" "firehose_delivery_policy" {
+  count  = local.count
+  name   = "allow_firehose_deliver_to_s3"
+  role   = aws_iam_role.firehose_delivery_role[0].id
+  policy = data.aws_iam_policy_document.firehose_delivery_policy_doc[0].json
+  tags   = var.tags
+}
+
+data "aws_iam_policy_document" "firehose_delivery_policy_doc" {
   count = local.count
   statement {
     sid    = "AllowFirehoseToDeliveryToS3"
