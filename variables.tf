95c1b4cd92b3882a42c099d1b6ba2c519fbf233f
Yi Zhang <yi@bravenewcoin.com>
2019-08-30 11:58:34 +1200

    1. Add data convert configuration to firehose 2. Add transform configuration to firehose 3. Merge firehose-variables into variables
    Resolve: #9

--- variables.tf
+++ variables.tf
@@ -99,6 +99,110 @@ variable "create_firehose" {
   default     = false
 }
 
+variable "kinesis-firehose_name" {
+  type        = string
+  description = "The name of kinesis firehose"
+  default     = ""
+}
 
+variable "s3_bucket_arn" {
+  type        = string
+  description = "The ARN of the S3 bucket"
+  default     = ""
+}
+
+variable "s3_bucket_prefix" {
+  type        = string
+  description = "The name of the folder to store data"
+  default     = ""
+}
+
+variable "s3_bucket_error_prefix" {
+  type        = string
+  description = "The name of the folder to store error data"
+  default     = ""
+}
+
+variable "buffer_size" {
+  type        = number
+  description = "Buffer incoming data to the specified size, in MBs, before delivering it to the destination"
+  default     = 128
+}
+
+variable "buffer_interval" {
+  type        = number
+  description = "Buffer incoming data for the specified period of time, in seconds, before delivering it to the destination"
+  default     = 900
+}
+
+variable "compression_format" {
+  type        = string
+  description = "The compression format"
+  default     = ""
+}
+
+variable "cloudwatch_log_enable" {
+  type        = bool
+  description = "Check if the cloudwatch log is enabled"
+  default     = true
+}
+
+variable "processing_configuration_enable" {
+  type        = bool
+  description = "Check if the data processing configuration is enabled"
+  default     = false
+}
+
+variable "processor_lambda_arn" {
+  type        = string
+  description = "The ARN of the processor lambda"
+  default     = ""
+}
+
+variable "data_format_conversion_enable" {
+  type        = bool
+  description = "Check if the data conversion is enabled"
+  default     = false
+}
+
+variable "firehose_alarm_evaluation_periods" {
+  type        = number
+  description = "The number of periods over which data is compared to the specified threshold."
+  default     = 1
+}
+
+variable "firehose_alarm_threshold" {
+  type        = number
+  description = "The value against which the specified statistic is compared."
+  default     = 0
+}
+
+variable "firehose_alarm_period" {
+  type        = number
+  description = "The period in seconds over which the specified statistic is applied."
+  default     = 300
+}
+
+variable "firehose_alarm_statistic" {
+  type        = string
+  description = "The statistic to apply to the alarm's associated metric. Either of the following is supported: SampleCount, Average, Sum, Minimum, Maximum"
+  default     = "Sum"
+}
+
+variable "aws_account_id" {
+  type        = string
+  description = "The aws account of id that creates resources"
+  default     = ""
+}
+
+variable "data_format_conversion_schema" {
+  type        = map(string)
+  description = "The schema configuration of glue table"
+  default     = {
+    "database_name" = ""
+    "table_name"    = ""
+    "role_arn"      = aws_iam_role.firehose_delivery_role[0].arn
+  }
+}
 
 
