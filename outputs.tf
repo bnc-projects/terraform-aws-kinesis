3cfdcca339d5d23a79508d47008d2137df316991
Bhavik Kumar <bhavik@bravenewcoin.com>
2020-03-06 13:13:59 +1300

    Remove firehose components from kinesis module

--- outputs.tf
+++ outputs.tf
@@ -5,11 +5,3 @@ output "kinesis_stream_arn" {
 output "kinesis_stream_name" {
   value = aws_kinesis_stream.default.name
 }
-
-output "kinesis_firehose_arn" {
-  value = aws_kinesis_firehose_delivery_stream.extended_s3_stream[0].arn
-}
-
-output "kinesis_firehose_name" {
-  value = aws_kinesis_firehose_delivery_stream.extended_s3_stream[0].name
-}
